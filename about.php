487f1a968bf49456ee860104569e825c821649dd
Justin Derby <justderb@iastate.edu>
2012-06-16 23:22:26 -0700

    TOC

--- about.php
+++ about.php
@@ -36,30 +36,39 @@
 			</div>
 			<div id="contentbody">
 				<div class="innerpading">
-					<h2>What is this site?</h2>
+					<ol style="font-size:1.5em;padding:0 0 0 20px">
+						<li><a href="#about1">What is this site?</a></li>
+						<li><a href="#about2">This site looks a lot like FitBit's website.  What gives?</a></li>
+						<li><a href="#about3">Why doesn't FitBit have a search feature?</a></li>
+						<li><a href="#about4">I just made a group and I can't find it on here!</a></li>
+						<li><a href="#about5">I got an issue or suggestion</a></li>
+						<li><a href="#about6">Why is this website called Relliker.com?</a></li>
+					</ol>
+					<div style="height:20px"></div>
+					<h2 id="about1">What is this site?</h2>
 					<p>This is a website to help FitBit users easily search the fitbit 
 					community 
 					groups so that they don't have to wade through pages and pages of groups only 
 					to get nowhere!</p>
 					<br />
-					<h2>This site looks a lot like FitBit's website.  What gives?</h2>
+					<h2 id="about2">This site looks a lot like FitBit's website.  What gives?</h2>
 					<p>The reason why this site looks so alike is because it is! I have taken and 
 					reverse-engineered their website to create a look-alike website so that any FitBit 
-					user know immediately how to the site.  <b>This, in no way, is a phishing website.</b>
+					user know immediately how to the site.  <strong>This, in no way, is a phishing website.</strong>
 					We don't even ask you to log in anywhere!</p>
 					<br />
-					<h2>Why doesn't FitBit have a search feature?</h2>
+					<h2 id="about3">Why doesn't FitBit have a search feature?</h2>
 					<p>I don't know!  You should ask them!</p>
 					<br />
-					<h2>I just made a group and I can't find it on here!</h2>
+					<h2 id="about4">I just made a group and I can't find it on here!</h2>
 					<p>I scrape FitBit's server about everyday so please wait at least 24 hours for 
 					your group will show up.</p>
 					<br />
-					<h2>I got an issue or suggestion</h2>
+					<h2 id="about5">I got an issue or suggestion</h2>
 					<p>Great! Head on over to <a href="http://www.fitbit.com/group/229YCB">FitBit Group 
 					Search</a> and create a topic discussion.</p>
 					<br />
-					<h2>Why is this website called Relliker.com?</h2>
+					<h2 id="about6">Why is this website called Relliker.com?</h2>
 					<p>I don't know.  It was just a domain name I bought... I'll buy a better one eventually.</p>
 
 				</div>
