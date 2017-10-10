b99f1de46ec95b65875466029a5cdbdd1e4a278d
Justin Derby <justderb@gmail.com>
2017-10-09 21:21:53 -0700

    Publish success metric to CloudWatch when done

--- python/scraper/ec2_runscript.sh
+++ python/scraper/ec2_runscript.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -x
 # Needs to be run before this script executes:
 #   sudo yum install -y build-dep python-psycopg2 postgresql-devel gcc
 
@@ -10,7 +10,20 @@ aws s3 cp s3://relliker.com/scraper/relliker-settings.source relliker-settings.s
 source relliker-settings.source
 aws s3 cp s3://relliker.com/scraper/group_scraper-1.0.0.tar.gz group_scraper-1.0.0.tar.gz
 pip install group_scraper-1.0.0.tar.gz
-python -m group_scraper || true
+python -m group_scraper
+if [ $? == 0 ]; then
+  aws cloudwatch put-metric-data --region=us-west-2 \
+      --metric-name "Success" \
+      --namespace "Relliker/Scraper"\
+      --value 1 \
+      --timestamp $(date --utc +%FT%T.%3NZ)
+else
+  aws cloudwatch put-metric-data --region=us-west-2 \
+      --metric-name "Success" \
+      --namespace "Relliker/Scraper"\
+      --value 0 \
+      --timestamp $(date --utc +%FT%T.%3NZ)
+fi
 popd
 
 sudo shutdown -h now
